fb4c7edef3554b863a11ff386405472f00387056
Michele Scarlato <michele.scarlato@gmail.com>
2021-08-24 15:18:29 +0900

    defined first 3 main steps.

---
+++ 1.git_clone.sh
@@ -0,0 +1,5 @@
+cd ~
+mkdir gitrepo-endocode/
+cd gitrepo-endocode/
+git clone git@github.com:fasten-project/LCV-CM.git
+git checkout maven-use-case
---
+++ 3.pypi_license_extractor.sh
@@ -0,0 +1,2 @@
+cd ~/gitrepo-endocode/LCV-CM/src/LCV/
+python3 python3 PYPI_license_collector.py
